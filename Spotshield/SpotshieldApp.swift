e0c1dfd4b6a3ff298c0a6688432f9066b5bedc15
cameronkoons <koonscam@msu.edu>
2024-01-27 13:59:46 -0500

    Initial Commit

---
+++ Spotshield/SpotshieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotshieldApp.swift
+//  Spotshield
+//
+//  Created by Cameron Koons on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotshieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
